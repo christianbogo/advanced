7402c5660997f8eb14d5aa0b34a08de3111be1fb
Christian Cutter <christianbcutter@yahoo.com>
2025-04-24 15:28:51 -0700

    feat: integrate Firebase and add ESLint/Prettier configuration; enhance layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import './styles/layout.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="data-viewport">
+        <div className="data-viewport-column">
+          <div className="data-viewport-column-container">
+            <p>test 1</p>
+          </div>
+          <div className="data-viewport-column-container">
+            <p>test 2</p>
+          </div>
+          <div className="data-viewport-column-container">
+            <p>test 3</p>
+          </div>
+        </div>
+        <div className="data-viewport-column">
+          <div className="data-viewport-column-container">
+            <p>test 4</p>
+          </div>
+          <div className="data-viewport-column-container">
+            <p>test 5</p>
+          </div>
+        </div>
+        <div className="data-viewport-column">
+          <div className="data-viewport-column-container">
+            <p>test 6</p>
+          </div>
+          <div className="data-viewport-column-container">
+            <p>test 7</p>
+          </div>
+        </div>
+      </div>
+      <div className="form-viewport">
+        <p>test 8</p>
+      </div>
     </div>
   );
 }
